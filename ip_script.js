a00cfad4a7a693aad4a1417595497473e130c630
Casey Ydenberg <ydenberg@gmail.com>
2014-01-03 20:25:00 -0500

    Removed console.log

--- ip_script.js
+++ ip_script.js
@@ -5,7 +5,6 @@ jQuery(document).ready(function() {
 	//to 1.5 * the font size, if font is large.
 	fs_str = jQuery('.impactpubs_publication').css('font-size');
 	fs_int = parseInt(fs_str);
-	console.log(fs_int * 1.5);
 	if ( fs_int * 1.5 > 23 ) {
 		lh_int = fs_int * 1.5;
 		lh_str = lh_int + 'px'
